0b133ad9cd2f1d2a145978b6b92b12617604d31f
zq992357666 <993572666@qq.com>
2014-03-06 21:14:33 +0800

    111111111

--- ehcacheaop/src/com/ehcache/Testehcache.java
+++ ehcacheaop/src/com/ehcache/Testehcache.java
@@ -19,10 +19,13 @@ public class Testehcache {
 	  public void test(){
 		  ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext-e.xml");  
 	    	 EhCacheCacheManager methodCache=(EhCacheCacheManager) context.getBean("methodCache");
-	    	 cacheManager=methodCache.getCacheManager();
+	    	 CacheManager cacheManager=methodCache.getCacheManager();
+	    	 EhCacheCacheManager methodCache1=(EhCacheCacheManager) context.getBean("methodCache");
+	    	 CacheManager cacheManager1=methodCache1.getCacheManager();
+	    	 System.out.println(cacheManager1==cacheManager);
 	    	 echecha();
 	    	 System.out.println("----------");
-	    	 echecha();
+	    	 echecha1();
 		  
 	  }
 	  public void echecha(){
@@ -38,4 +41,17 @@ public class Testehcache {
 		    		System.out.println("element   " +element.getObjectKey());
 	    	 }
 	  }
+	  public void echecha1(){
+			  
+			  Cache cache= cacheManager.getCache("test111");
+		    	 if(cache==null){
+		    			Cache addcache = new Cache("test111", 150, true, false, 5, 2);
+		    			cacheManager.addCache(addcache);
+		    			addcache.put(new Element("key1","value1"));
+		    	 }else{
+		    		 cache= cacheManager.getCache("test111");
+		    			Element element = cache.get("key1");
+			    		System.out.println("element   " +element.getObjectKey());
+		    	 }
+		  }
 }
